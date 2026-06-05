878700d32f11c0c8cf9a58739c1190f251727da0
minningminfay-cloud <minningminfay@gmail.com>
2026-06-05 14:32:19 +0700

    First upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "meter-app-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./rate1115.html",
+  "./rate1125.html",
+  "./rate2125.html",
+  "./rate8115.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
